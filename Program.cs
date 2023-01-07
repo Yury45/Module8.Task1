3e4377a5d52040f1cd4c9d4e45f0ed76326e7264
Yury <zm.ptz.ru2010@yandex.ru>
2023-01-07 15:53:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Module8.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"C:\Downloads\123";
+
+            ClearDirectory(Path);
+
+        }
+
+        static void ClearDirectory(string path)
+        {
+            try
+            {
+                DirectoryInfo directory = new DirectoryInfo(path);
+
+                if (Directory.Exists(path))
+                {
+
+                    foreach (var dir in directory.GetDirectories())
+                    {
+
+                        if ((DateTime.Now - Directory.GetLastAccessTime(dir.FullName)).TotalMinutes > 30)
+                        {
+                            Directory.Delete(dir.FullName, true);
+                        }
+                        else
+                        {
+                            ClearDirectory(dir.FullName);
+                        }
+                    }
+
+                    foreach (var file in directory.GetFiles())
+                    {
+
+                        if ((DateTime.Now - Directory.GetLastAccessTime(file.FullName)).TotalMinutes > 30)
+                        {
+                            File.Delete(file.FullName);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Директория {path} отсутствует.");
+                }
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Директория не обнаружена! {e.Message}");
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine($"Отсутствует доступ! {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Возникла ошибка: {e.Message}");
+            }
+        }
+    }
+}
